5277eebe540dfb61f5257b3be00ad725cda236a1
polonity <hiken.pc@gmail.com>
2023-04-16 01:18:32 +0900

    🌟Add verify hashK_A equal to hashK_UA (at contract)

--- physicalAsset/server/app.py
+++ physicalAsset/server/app.py
@@ -11,6 +11,8 @@ import base64
 import json
 import logging
 import subprocess
+from web3 import Web3
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -24,6 +26,9 @@ env = {}
 # read .env file
 PUBLIC_KEY = '0x3b3f9b38988da8941bf98c44791e42d92bfea57a7cc371c6740d5d68d71f2f6b0c3dab7179a1ba434cab6d52edc227e98aefd90d9a8615a68e92c94ce0025af3'
 PRIVATE_KEY = '0xc340a2006b4e66676bab00fd91d083d76f5af46707e2d3b61ead3ef497273636'
+CONTRACT_ADDRESS = '0x49477C47C9627a769c2374B59e436fc406509aCA'
+gethashK_UA_ABI = '[{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"}],"stateMutability":"view","type":"function","name":"gethashK_UA","outputs":[{"internalType":"uint256","name":"","type":"uint256"}]}]'
+
 # PUBLIC_KEY = os.environ['PUBLIC_KEY']
 # PRIVATE_KEY = os.environ['PRIVATE_KEY']
 # CONTRACT_ADDRESS = os.environ['CONTRACT_ADDRESS']
@@ -33,13 +38,23 @@ logger.info('PRIVATE_KEY: ' + PRIVATE_KEY)
 # logger.info('CONTRACT_ADDRESS: ' + CONTRACT_ADDRESS)
 
 AUDIO_FILE_NAME = './decrypted_audio.webm'
-RADIO_COMMAND = f'ffmpeg -i  {AUDIO_FILE_NAME} -acodec pcm_s16le -ar 22050 -f wav - | sudo /home/gohiki/workspace/radio/pifm -'
+RADIO_COMMAND = f'ffmpeg -i  {AUDIO_FILE_NAME} -acodec pcm_s16le -ar 22050 -f wav - | sudo ~/.worldradio/pifm -'
 
 # set publickey to contract if have not set yet
 
 
+# Ethereumノードに接続
+w3 = Web3(Web3.HTTPProvider('https://polygon-mumbai.g.alchemy.com/v2/cFpM9PtWSf3FQfrgO6Dsf6sPXqPUwdpM'))
+
 curve = registry.get_curve('secp256k1')
 
+def gethashK_UA(tokenId: int):
+    # contract interface
+    contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=gethashK_UA_ABI)
+
+    # contract call
+    hashAsInt = contract.functions.gethashK_UA(tokenId).call()
+    return '0x' + hex(hashAsInt)[2:].zfill(64)
 
 def calcSharedKey(publicKeyHex: str, privateKeyHex: str) -> str:
 
@@ -99,8 +114,11 @@ def upload_file():
     logger.info('SHARED KEY HASH\t: %s', shared_key_hash_hex)
 
     # !check shared key hash is same as contract
-    # if shared_key_hash_hex != CONTRACT_ADDRESS:
-    #     return jsonify({'message': 'error: shared key hash is not same as contract address!'})
+    hashK_UA = gethashK_UA(int(user, 16))
+    logger.info('hashK_UA\t: %s', hashK_UA)
+
+    if shared_key_hash_hex != hashK_UA:
+        return jsonify({'message': 'error: shared key hash is not same as contract address!'})
 
     # decrypt data
     iv = audio_as_encrypted[:16]
@@ -120,7 +138,7 @@ def upload_file():
                     AUDIO_FILE_NAME, len(decrypted_compressed_data))
 
     # execute system command
-    subprocess.call(RADIO_COMMAND, shell=True)
+    subprocess.Popen(RADIO_COMMAND, shell=True)
 
     return jsonify({'message': 'success!'})
 
